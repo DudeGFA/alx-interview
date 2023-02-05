2a545309f5e9ea03d71ccd006a06ce75d317d2ec
God'sfavour Idowu-Agida <favouridowuagida5@gmail.com>
2023-02-05 23:22:00 +0100

    improved time complexity of canUnlockall in 0-lockboxes.py
    Switched to a recursive function

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -4,6 +4,14 @@
 """
 
 
+def recursiveUnlock(boxes, index, unlockableBoxes):
+    for key in boxes[index]:
+        if key < len(unlockableBoxes) and unlockableBoxes[key] == 0:
+            unlockableBoxes[key] = 1
+            unlockableBoxes = recursiveUnlock(boxes, key, unlockableBoxes)
+    return unlockableBoxes
+
+
 def canUnlockAll(boxes):
     """"
         Checks if all boxes in a list of list
@@ -17,12 +25,16 @@ def canUnlockAll(boxes):
     for box in boxes:
         unlockableBoxes.append(0)
     unlockableBoxes[0] = 1
-    for i in range(len(boxes)):
-        for box in boxes:
-            if unlockableBoxes[boxes.index(box)] == 1:
-                for key in box:
-                    if key < len(unlockableBoxes):
-                        unlockableBoxes[key] = 1
+    for key in boxes[0]:
+        if key < len(unlockableBoxes):
+            unlockableBoxes[key] = 1
+            unlockableBoxes = recursiveUnlock(boxes, key, unlockableBoxes)
+    # for i in range(len(boxes)):
+     #   for box in boxes:
+      #      if unlockableBoxes[boxes.index(box)] == 1:
+       #         for key in box:
+        #            if key < len(unlockableBoxes):
+         #               unlockableBoxes[key] = 1
 
     for key in unlockableBoxes:
         if key != 1:
