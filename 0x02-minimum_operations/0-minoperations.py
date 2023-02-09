1a5c83e4c85dd65a2daead2851c50d87f3157b40
God'sfavour Idowu-Agida <favouridowuagida5@gmail.com>
2023-02-09 13:17:22 +0100

    Solved max_operations code problem

---
+++ 0x02-minimum_operations/0-minoperations.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""
+    Contains function minOperations
+"""
+
+
+def minOperations(n):
+    """
+        calculates the fewest number of operations
+        needed to result in exactly n H characters
+        in a file
+    """
+    if (n <= 1):
+        return 0
+    i = 2
+    while (i <= n):
+        while(n % i == 0):
+            res += i
+            n = n / i
+        i += 1
+    return res
