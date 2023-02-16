fa6e4bbb236abfc5231b883aa90a1dade072e497
God'sfavour Idowu-Agida <favouridowuagida5@gmail.com>
2023-02-17 00:52:42 +0100

    create 0-stats.py, test.py

---
+++ 0x03-log_parsing/0-stats.py
@@ -0,0 +1,42 @@
+"""
+    reads stdin line by line
+    and computes metrics
+"""
+import sys
+line_count = 1
+
+status_codes = {"200": 0, "301": 0, "400": 0, "401": 0,
+                "403": 0, "404": 0, "405": 0, "500": 0}
+total_file_size = 0
+
+
+def print_statistics():
+    """
+        Prints log statistics
+    """
+    print("File size: ", str(total_file_size))
+    for key, value in status_codes.items():
+        if value != 0:
+            print(f"{key}: {value}")
+
+
+try:
+    for line in sys.stdin:
+        line_count += 1
+        split_line = line.split()
+        try:
+            file_size = int(split_line[-1])
+        except Exception:
+            continue
+        try:
+            status_code = int(split_line[-2])
+            status_codes[str(status_code)] += 1
+        except Exception:
+            pass
+        total_file_size += file_size
+        if line_count % 10 == 0:
+            print_statistics()
+        print_statistics
+except KeyboardInterrupt:
+    print_statistics
+    raise
--- 0x03-log_parsing/README.md
+++ 0x03-log_parsing/README.md
@@ -1,5 +1,5 @@
 # Tasks
-0. Log parsing          
+### 0. Log parsing          
 **mandatory**               
 Write a script that reads `stdin` line by line and computes metrics:
 
