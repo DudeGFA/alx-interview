6b5317f606d0d568c64244eab27cc6352193661e
God'sfavour Idowu-Agida <favouridowuagida5@gmail.com>
2023-02-17 01:29:35 +0100

    Update 0-stats.py

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -4,8 +4,9 @@
     and computes metrics
 """
 from sys import stdin
-line_count = 0
 
+
+line_count = 0
 status_codes = {"200": 0, "301": 0, "400": 0, "401": 0,
                 "403": 0, "404": 0, "405": 0, "500": 0}
 total_file_size = 0
