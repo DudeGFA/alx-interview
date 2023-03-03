323e65308f988a939c283bd881f351699f091883
God'sfavour Idowu-Agida <favouridowuagida5@gmail.com>
2023-03-03 01:20:13 +0100

    Update 0-nqueens.py

--- 0x05-nqueens/0-nqueens.py
+++ 0x05-nqueens/0-nqueens.py
@@ -60,7 +60,17 @@ def main():
     """
         main program
     """
-    board_size = int(sys.argv[1])
+    if len(sys.argv) != 2:
+        print("Usage: nqueens N")
+        exit(1)
+    try:
+        board_size = int(sys.argv[1])
+    except ValueError:
+        print("N must be a number")
+        exit(1)
+    if board_size < 4:
+        print("N must be at least 4")
+        exit(1)
     board = [[0 for i in range(board_size)] for j in range(board_size)]
     findsolution(0, board, board_size)
 
