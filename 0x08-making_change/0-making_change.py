c254c282ab6d3c422feae9127befb106e07af598
God'sfavour Idowu-Agida <favouridowuagida5@gmail.com>
2023-03-22 23:10:04 +0100

    Update 0-making_change.py

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -19,13 +19,20 @@ def makeChange(coins, total):
     """
     if (total == 0):
         return 0
+    table = [0 for i in range(total + 1)]
 
-    count = sys.maxsize
+    table[0] = 0
 
-    for i in range(0, len(coins)):
-        if (coins[i] <= total):
-            prev_count = makeChange(coins, total - coins[i])
-            if (prev_count + 1 < count and prev_count != sys.maxsize):
-                count = prev_count + 1
+    for i in range(1, total + 1):
+        table[i] = sys.maxsize
 
-    return count
+    for i in range(1, total + 1):
+
+        for j in range(len(coins)):
+            if (coins[j] <= i):
+                prev_count = table[i - coins[j]]
+                if (prev_count != sys.maxsize and prev_count + 1 < table[i]):
+                    table[i] = prev_count + 1
+    if (table[total] == sys.maxsize):
+        return -1
+    return table[total]
